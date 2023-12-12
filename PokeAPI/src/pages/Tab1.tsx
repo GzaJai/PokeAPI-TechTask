facc2d645e562141a0423f4d0f88aa595c3aee23
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2023-12-12 18:05:22 -0300

    adding base grid
    ading the base grid for the pokemon's card display

--- PokeAPI/src/pages/Tab1.tsx
+++ PokeAPI/src/pages/Tab1.tsx
@@ -1,13 +1,24 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, { useEffect, useState} from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCol, IonGrid, IonRow, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 import './Tab1.css';
+import { nameCapitalize } from '../utils/utils';
 
 const Tab1: React.FC = () => {
+
+  const [pokeData, setPokeData] = useState();
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=1292')
+    .then(response => response.json())
+    .then(data => setPokeData(data));
+  }, [])
+  
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Pocketdex</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +27,43 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonGrid fixed={true}>
+          <IonRow>
+            {pokeData && console.log(pokeData)}
+            {pokeData && pokeData.results.map((poke, index) =>(
+              <IonCol key={index}>
+                <IonCard>
+                <img alt={`imagen de ${poke.name}`}  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index + 1}.png`} />
+                  <IonCardHeader>
+                    <IonCardTitle>{nameCapitalize(poke.name)}</IonCardTitle>
+                  </IonCardHeader>
+                </IonCard>
+              </IonCol>
+            ))}
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonCard>
+                <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+                <IonCardHeader>
+                  <IonCardTitle>Card Title</IonCardTitle>
+                  <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                </IonCardHeader>
+                <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+              </IonCard>
+            </IonCol>
+            <IonCol>
+              <IonCard>
+                <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+                <IonCardHeader>
+                  <IonCardTitle>Card Title</IonCardTitle>
+                  <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                </IonCardHeader>
+                <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
