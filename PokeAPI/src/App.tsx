f5d4b044adb2e6f49b06314c7bdc5917f8e2bd7f
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2023-12-13 17:10:40 -0300

    deleting imports
    deleting unnecessary imports

--- PokeAPI/src/App.tsx
+++ PokeAPI/src/App.tsx
@@ -1,12 +1,7 @@
 import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
-  IonIcon,
-  IonLabel,
   IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
